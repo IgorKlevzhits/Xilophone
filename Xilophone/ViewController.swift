794936750c2b2d8b8309b41d1573c36ed421a39c
Igor <klevzhits94@icloud.com>
2023-01-27 06:28:38 +0300

    Add all buttons

--- Xilophone/ViewController.swift
+++ Xilophone/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  Xilophone
-//
-//  Created by Игорь Клевжиц on 27.01.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let buttonsView = XilophoneButtons()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = .white
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        view.addSubview(buttonsView)
     }
 
+    
 
 }
 
+extension ViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            buttonsView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            buttonsView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            buttonsView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            buttonsView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+    
+}
---
+++ Xilophone/XilophoneButtons.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+class XilophoneButtons: UIView {
+    
+    private let dictanseEdgesButton: CGFloat = 10
+    private let cornerRadiusButton: CGFloat = 15
+    private let fontSizeButton: CGFloat = 50
+    
+    private var buttons: [UIButton] = []
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        setButtons()
+        setupButtons()
+        setConstraints()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupButtons() {
+        for button in buttons {
+            self.addSubview(button)
+        }
+    }
+    
+    private func setButtons() {
+        let titleButtons = [ "C", "D", "E", "F", "G", "A", "B" ]
+        let colorButtons = [ UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.cyan, UIColor.blue, UIColor.purple]
+        for index in 0..<7 {
+            let button = UIButton(type: .system)
+            button.backgroundColor = colorButtons[index]
+            button.layer.cornerRadius = cornerRadiusButton
+            button.setTitle(titleButtons[index], for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: fontSizeButton)
+            button.tintColor = .white
+            button.translatesAutoresizingMaskIntoConstraints = false
+            buttons.append(button)
+        }
+    }
+}
+
+extension XilophoneButtons {
+    
+    private func setConstraints() {
+        
+        
+        NSLayoutConstraint.activate([
+            buttons[0].topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            buttons[0].leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: dictanseEdgesButton),
+            buttons[0].trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -dictanseEdgesButton),
+            buttons[0].bottomAnchor.constraint(equalTo: buttons[1].topAnchor, constant: -10)
+        ])
+        for index in 1..<6 {
+            NSLayoutConstraint.activate([
+                buttons[index].leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: dictanseEdgesButton * CGFloat(index + 1)),
+                buttons[index].trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -dictanseEdgesButton * CGFloat(index + 1)),
+                buttons[index].bottomAnchor.constraint(equalTo: buttons[index + 1].topAnchor, constant: -10)
+            ])
+        }
+        NSLayoutConstraint.activate([
+            buttons[6].leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: dictanseEdgesButton * 7),
+            buttons[6].trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -dictanseEdgesButton * 7),
+            buttons[6].bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0)
+        ])
+        for index in 0..<6 {
+            NSLayoutConstraint.activate([
+                buttons[index].heightAnchor.constraint(equalTo: buttons[index + 1].heightAnchor, multiplier: 1)
+            ])
+        }
+    }
+}
